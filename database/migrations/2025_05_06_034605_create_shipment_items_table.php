7875682e50f6ce2d097ca74bb1f6f9f1be3f29f9
jeffkipjeff <jeffkip354@gmail.com>
2025-05-06 11:03:56 +0300

    added service level feature

---
+++ database/migrations/2025_05_06_034605_create_shipment_items_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('shipment_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('shipment_id');
+            $table->string('item_name');
+            $table->integer('packages_no');
+            $table->integer('weight');
+            $table->integer('length')->nullable();
+            $table->integer('height')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('shipment_items');
+    }
+};
