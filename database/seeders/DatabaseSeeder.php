0cc0f9a8276ae6acad616fdbb67fc3ab1dc04e58
mwaele <emmanuelmwaele@gmail.com>
2025-07-08 09:37:13 +0300

    Modified dashboard, Added seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,14 +15,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $this->call([
+            UserSeeder::class,
+            OfficeSeeder::class,
+            CompanyInfoSeeder::class,
+            VehicleSeeder::class,
         ]);
-        $this->call(OfficeSeeder::class);
-        $this->call(CompanyInfoSeeder::class);
-        $this->call(UserSeeder::class);
     }
 }
