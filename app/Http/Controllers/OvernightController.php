69e0307d8e21a4f7056d586a2470e5afd7301601
mwaele <emmanuelmwaele@gmail.com>
2025-07-18 11:21:04 +0300

    Began on modal creation

--- app/Http/Controllers/OvernightController.php
+++ app/Http/Controllers/OvernightController.php
@@ -3,15 +3,43 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use App\Models\Client;
 use App\Models\ClientRequest;
 use App\Models\SubCategory;
+use App\Models\ShipmentCollection;
+use App\Models\Vehicle;
+use App\Models\User;
 
 class OvernightController extends Controller
 {
     //
     public function on_account()
     {
+        // Determine the correct CAST expression based on DB driver
+        $driver = DB::getDriverName();
+        $castExpression = $driver === 'pgsql'
+            ? 'CAST(SUBSTRING("requestId" FROM 5) AS INTEGER)'
+            : 'CAST(SUBSTRING(requestId, 5) AS UNSIGNED)';
+
+        // Generate Unique Request ID
+        $lastRequestFromClient = ClientRequest::where('requestId', 'like', 'REQ-%')
+            ->orderByRaw("$castExpression DESC")
+            ->value('requestId');
+
+        $lastRequestFromCollection = ShipmentCollection::where('requestId', 'like', 'REQ-%')
+            ->orderByRaw("$castExpression DESC")
+            ->value('requestId');
+
+        $clientNumber = $lastRequestFromClient ? (int)substr($lastRequestFromClient, 4) : 0;
+        $collectionNumber = $lastRequestFromCollection ? (int)substr($lastRequestFromCollection, 4) : 0;
+        $nextNumber = max(max($clientNumber, $collectionNumber) + 1, 10000);
+        $request_id = 'REQ-' . str_pad($nextNumber, 5, '0', STR_PAD_LEFT);
+
+        $clients = Client::where('type', 'on_account')->get();
+        $vehicles = Vehicle::all();
+        $drivers = User::where('role', 'driver')->get();
+
         $overnightSubCategoryIds = SubCategory::where('sub_category_name', 'Overnight')->pluck('id');
 
         $clientRequests = ClientRequest::whereIn('sub_category_id', $overnightSubCategoryIds)
@@ -22,7 +50,7 @@ class OvernightController extends Controller
             ->get();
 
 
-        return view('same_day.walk_in', compact('clientRequests'));
+        return view('overnight.on-account', compact('clients', 'clientRequests', 'request_id', 'vehicles', 'drivers'));
     }
 
     public function walk_in()
